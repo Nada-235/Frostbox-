c342921cf3032be0518ed6355a0c670a9e3281a6
Nada ✨ <136603758+Nada-235@users.noreply.github.com>
2026-09-24 10:42:45 +0300

    Register Frostbox service worker in production

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,8 @@ createRoot(document.getElementById('app')).render(
     <App />
   </StrictMode>
 );
+
+
+if('serviceWorker' in navigator && import.meta.env.PROD){
+  window.addEventListener('load', () => navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).catch(console.error));
+}
